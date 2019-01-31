7aa90710ad04c7445e1ad48c2488e27a2a90e9c7
Cory Salmon <salmocor@sheridancollege.ca>
2019-01-31 09:03:40 -0500

    This is the First commit

---
+++ src/testfortodaysinclass/Main.java
@@ -0,0 +1,17 @@
+/*
+    Name: Cory Salmon
+    Assignment:
+    Program: PROG24178/Object Oriented Programming 2: Java
+    Date:
+    Description:
+*/
+
+package testfortodaysinclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+    }
+
+}

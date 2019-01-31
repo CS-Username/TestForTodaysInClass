d07679a1ec74a0fc98aea3a1ca0e689650ca27ce
Cory Salmon <salmocor@sheridancollege.ca>
2019-01-31 09:07:59 -0500

    This is the First commit

--- src/testfortodaysinclass/Main.java
+++ src/testfortodaysinclass/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is a test");
+        System.out.println("Here is a revision");
+        System.out.println("Here is another line of text so I can edit one and delete the other");
     }
 
 }
